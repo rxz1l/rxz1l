d82b777deb4de7cd4151914dc1c480dea95e9388
renxz <409368950@qq.com>
2023-07-18 15:35:56 +0800

    新增celltalker运行脚本

---
+++ celltalker/celltalker.sh
@@ -0,0 +1,9 @@
+/public/work/Personal/renxiaozhen/software/miniconda3/envs/R/bin/Rscript /public/work/Personal/renxiaozhen/celltalker/celltalker_data.r \
+    -d /public/work/Personal/renxiaozhen/rice.Rds \
+    -l /public/work/Personal/renxiaozhen/celltalker/rice_db.Rdata \
+    -g cluster \
+    -P TRUE \
+    -m Average 
+
+
+
